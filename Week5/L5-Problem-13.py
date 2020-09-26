949b2bd44fb6d5fece4c1b9906924551f42edcbf
HolyLoki <forwoermind@gmail.com>
2020-09-26 18:17:54 +0600

    5

---
+++ Week5/L5-Problem-13.py
@@ -0,0 +1,15 @@
+a = input()
+b = a.strip()
+list_input = b.split(" ")
+
+list_int = []
+
+for i in range(len(list_input)):
+    list_int.append(int(list_input[i]))
+
+def sorted(numbers):
+    numbers.sort()
+    print(numbers)
+
+
+sorted(list_int)
